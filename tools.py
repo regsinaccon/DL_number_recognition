6aae62440790714a4fe8aea2ea4ebc1cde158104
regsinaccon <116173314+regsinaccon@users.noreply.github.com>
2024-03-23 16:05:09 +0800

    add some features

--- tools.py
+++ tools.py
@@ -9,9 +9,6 @@ def dot(vector1,vector2):
         sum += vector1[column]*vector2[column]
     return sum
 
-# def split_the_squar(squar):
-#     rows = squar.shape[0]
-#     splitted_squar = numpy.zeros(rows)
 
 def val_pass_weight(input_vector,weight_squar):
     rows = weight_squar.shape[0]
@@ -40,22 +37,37 @@ def one_hot_encoder(Num):
     encoded = numpy.zeros((10,))
     encoded[Num] = 1
     return encoded
-# 
+ 
 def Deviation_of(expected_code,pridict_code):
     return pridict_code-expected_code
-# 
+ 
+def Return_Num(vector):
+    max_val = -1000
+    the_index = 0
+    for i in range(len(vector)):
+        if vector[i]>max_val:
+            max_val = vector[i]
+            the_index = i    
+    return the_index
 
-# def Pridict_acc()
 
 
 
-# maybe not so useful in b deviation
-def Sigma(*args):
-    sum = 0
-    for num in args:
-        sum += num
-    return sum
-
+# the testx contains only 128 column while w1 has 129 column and so does the things in the hidden layer
+# cause dot error
+def Predict_acc(testx,testy,w1,w2,test_size):
+    acc = 0
+    for i in range(test_size):
+        Img = numpy.append(testx[i],1)
+        a = val_pass_weight(Img,w1)
+        a = sigmoid(a)
+        b = numpy.append(a,1)
+        b = val_pass_weight(b,w2)
+        b = softmax(b)
+        Number = Return_Num(b)
+        if Number == testy[i]:
+            acc +=1
+    return acc/test_size
 
 
 @numba.jit(nopython=True)
@@ -63,11 +75,15 @@ def Append_one(vector):
     vector = numpy.append(vector,1)
     return vector
 
-'''
-unfinished function
-'''
-def Deviation_b(Deviation_Code,w2):
+
+def Deviation_b(Deviation_Code,w2,b):
     r = numpy.zeros((129,))
     for i in range(129):
+        tmp_sum = 0
         for j in range(10):
-            pass
+            tmp_sum += Deviation_Code[j]*w2[j][i]
+        r[i] = b[i]*(1-b[i])*tmp_sum
+    return r
+
+def revise_w1():
+    pass
