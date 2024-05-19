c62b1da4ceedb51a719b9be10d5540d4d3a3ce60
regsinaccon <116173314+regsinaccon@users.noreply.github.com>
2024-05-19 15:15:17 +0800

    Update main.py

--- New_model/main.py
+++ New_model/main.py
@@ -2,7 +2,5 @@ from cclass import *
 import numpy
 
 if __name__=='__main__':
-    m = Model(300,batchsize=512,learning_rate=0.01,momentum=(True,0.5),DLR=True,convolution=True)
+    m = Model(150)
     m.Train()
-    m.Show('loss') #loss or accuracy
-    m.check()
